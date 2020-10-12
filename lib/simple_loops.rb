73bccad92f8e3361d01fcd2bdfe77c314a1bd567
Zoe Kirsh <zoekirsh@gmail.com>
2020-10-12 01:41:17 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -14,8 +14,12 @@ def loop_message_n_times (message, number)
   end
 end
 
-def output_array (message, number)
-  
+def output_array (array)
+  counter = 0 
+  while counter < array.length do
+    puts array[counter]
+    counter += 1 
+  end
 end
 
 def return_string_array (array)
